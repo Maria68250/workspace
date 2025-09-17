7580ae32958d833c216d29f19aec1704a6898711
Maria68250 <martorres101507@gmail.com>
2025-09-17 16:20:26 -0500

    Delete Ejercicios Media tecnica directory

--- Ejercicios Media tecnica/ejercicio2.js
+++
@@ -1,4 +0,0 @@
-for(num = 1; num <= 100; num++){
-    console.log(num)
-}
-onsole.log(1, 2, 3, "hola mundo")
--- Ejercicios Media tecnica/ejercicio3.js
+++
@@ -1,7 +0,0 @@
-const nombres = ["Ana", "Sofia", "Jaider", "Esteban", "Thomas", "Maidy", "leoncio"]
-
-function Button(){
-    for (let i = 0; i <=6; i++) {
-        alert(nombres[i])
-    }
-}
